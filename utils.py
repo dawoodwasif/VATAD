0ef357878194db87e45cccc1c809ccbd56b1d146
RummanAli <45504456+RummanAli@users.noreply.github.com>
2023-11-29 18:28:21 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,25 +4,8 @@ import csv
 import cv2
 import os
 
-# def superimpose_mask(frame, mask,target_frame):
-#     """
-#     Pastes the mask in target_frame
-#     """
-#     # dest_path = "/home/ubuntu/Furqan/Rumman/VATAD/data/temp"
-#     # cv2.imwrite(os.path.join(dest_path,f'img_mask.jpg'),mask)
-#     # cv2.imwrite(os.path.join(dest_path,f'img_frmae.jpg'),frame)
-#     # cv2.imwrite(os.path.join(dest_path,f'img_tg_grame.jpg'),target_frame)
-#     print(target_frame.shape, mask.shape)
-#     target_frame_2 = target_frame.copy()
-#     q= frame * mask
-#     target_frame_2[mask==True] = q[q>0]
-#     return target_frame_2
-
 def superimpose_mask(frame, mask, target_frame):
-    """
-    Pastes the mask in target_frame
-    """
-    print(target_frame.shape, mask.shape)
+    # Pastes the mask in target_frame
     
     # Ensure mask is boolean
     mask = mask.astype(bool)
@@ -38,6 +21,7 @@ def superimpose_mask(frame, mask, target_frame):
 
 
 def update_annotation_file(annot_file,activity,frame_num,bbox,label,person_id):
+    # Adds new sample entry into the annotation file
     file = open(annot_file)
     writer = csv.writer(file)
     writer.writerow([activity,frame_num,bbox[0],bbox[1],bbox[2],bbox[3],label,person_id])
